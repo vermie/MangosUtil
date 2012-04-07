b54ffd5bcd972108aff3554e6bca217137f09488
faramir118 <faramir118@gmail.com>
2012-04-07 02:09:55 -0500

    OrCondtion and AndCondition classes

--- MangosUtil/Condition.cs
+++ MangosUtil/Condition.cs
@@ -44,5 +44,52 @@ namespace Mangos
             return string.Format("({0}, {1}, {2}, {3})", Id, Cond, Val1, Val2);
         }
     }
+
+    public abstract class BinaryCondition : ICondition
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public abstract int Cond { get; }
+
+        public int Val1 { get { return _lhs.Id; } }
+
+        public int Val2 { get { return _rhs.Id; } }
+
+        private ICondition _lhs;
+        private ICondition _rhs;
+
+        public BinaryCondition(ICondition lhs, ICondition rhs)
+        {
+            _lhs = lhs;
+            _rhs = rhs;
+        }
+
+        public sealed override string ToString()
+        {
+            return string.Format("({0}, {1}, {2}, {3})", Id, Cond, Val1, Val2);
+        }
+    }
+
+    public class OrCondition : BinaryCondition
+    {
+        public override int Cond { get { return -1; } }
+
+        public OrCondition(ICondition lhs, ICondition rhs) : base(lhs, rhs)
+        {
+        }
+    }
+
+    public class AndCondition : BinaryCondition
+    {
+        public override int Cond { get { return -2; } }
+
+        public AndCondition(ICondition lhs, ICondition rhs) : base(lhs, rhs)
+        {
+        }
+    }
 }
 
