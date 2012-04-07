f7d82f4980071bbf1d14415fa723f927d37c3558
faramir118 <faramir118@gmail.com>
2012-04-07 02:22:51 -0500

    write the example conditions to std out

--- MangosUtil/Runner.cs
+++ MangosUtil/Runner.cs
@@ -8,6 +8,9 @@ namespace Mangos
         {
             int id = 0;
             var expr = Parser.Parse(ref id, c => (c.HasAura(1234, 1) && c.IsHorde()) || (c.HasAura(1235, 1) && c.IsAlliance()));
+
+            foreach (var condtion in expr.Conditions)
+                Console.WriteLine(condtion);
         }
     }
 }
